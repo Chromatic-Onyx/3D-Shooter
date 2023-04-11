ca4136e865ea57aa83872eabfb70bce9fd9cdfbc
Peypug <Peypug3@gmail.com>
2023-04-10 20:59:30 -0700

    Add basic firing + Crosshair

---
+++ 3D-Shooter/Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private float range = 100f;
+    // private float damage = 10f;
+
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootGun();
+        }
+    }
+    void ShootGun()
+    {
+        RaycastHit hits;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hits, range))
+        {
+            Debug.Log(hits.transform.name);
+        }
+    }
+}
