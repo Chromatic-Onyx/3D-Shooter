06c66e19d215583b788f4fdb83ffc9af6743932a
Peypug <Peypug3@gmail.com>
2023-04-14 13:51:34 -0700

    Add recoil

--- 3D-Shooter/Assets/Scripts/Shoot.cs
+++ 3D-Shooter/Assets/Scripts/Shoot.cs
@@ -13,7 +13,16 @@ public class Shoot : MonoBehaviour
     public int ammo = 40;
     public int magSize = 40;
     public int ammoStash = 999;
-    
+
+    public GameObject gun2;
+    public float smooth = 5f;
+
+    public int rotateCount = 0;
+    public bool justShot = false;
+    int rotateScale = 1;
+    public float verticalRecoil = 0.5f;
+    public float horizontalRecoil = 0.5f;
+
     // Update is called once per frame
     void Update()
     {
@@ -29,14 +38,21 @@ public class Shoot : MonoBehaviour
             {
                 lastFired = Time.time;
                 ShootGun();
+                justShot = true;
+
             }
 
+        }
+        else if (justShot && !Input.GetButton("Fire1"))
+        {
+            stopRecoil();
         }
     }
 
     void ShootGun()
     {
         muzzleFlash.Play();
+        recoil();
         ammo -= 1;
         RaycastHit hits;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hits, range))
@@ -48,4 +64,40 @@ public class Shoot : MonoBehaviour
             }
         }
     }
+
+    void recoil()
+    {
+        rotateScale++;
+        Vector3 rotationVector = new Vector3(0f, 0f, -verticalRecoil);
+        gun2.transform.Rotate(rotationVector);
+        if (rotateScale % 2 == 1)
+        {
+            gun2.transform.Rotate(new Vector3(0f, horizontalRecoil, 0f));
+        }
+        else if (rotateScale % 2 == 0)
+        {
+            gun2.transform.Rotate(new Vector3(0f, -horizontalRecoil, 0f));
+        }
+        rotateCount += 1;
+        
+    }
+
+    void stopRecoil()
+    {
+        
+        gun2.transform.Rotate(new Vector3(0f, 0f, verticalRecoil * rotateCount));
+        if (rotateScale % 2 == 1)
+        {
+            gun2.transform.Rotate(new Vector3(0f, -horizontalRecoil, 0f));
+        }
+        else if (rotateScale % 2 == 0)
+        {
+            gun2.transform.Rotate(new Vector3(0f, horizontalRecoil, 0f));
+        }
+        justShot = false;
+        rotateCount = 0;
+        rotateScale = 0;
+    }
+
+   
 }
