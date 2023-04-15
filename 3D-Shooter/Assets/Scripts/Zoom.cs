4a975e7c6b1af7e9c93f1dda7da7e3329f1704ab
Peypug <Peypug3@gmail.com>
2023-04-14 19:30:20 -0700

    Add zoom to main gun

--- 3D-Shooter/Assets/Scripts/Zoom.cs
+++ 3D-Shooter/Assets/Scripts/Zoom.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class Zoom : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Animator gun2Animator;
+    public Camera Camera;
+    public bool canZoomOut = false;
+    public bool isAiming;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            gun2Animator.SetBool("isAiming", true);
+            Invoke("zoomin", 0.3f);
+            isAiming = true;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1) && canZoomOut)
+        {
+            gun2Animator.SetBool("isAiming", false);
+            isAiming = false;
+            zoomout();
+            canZoomOut = false;
+        }
+    }
+
+    void zoomin()
+    {
+        Camera.fieldOfView = 30;
+        canZoomOut = true;
+    }
+    void zoomout()
+    {
+        Camera.fieldOfView = 50;
     }
 }
